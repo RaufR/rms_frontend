62b95d35a94b58f5efb65dbaec13eb2eda27d39f
RaufR <rauf.rahmancz@gmail.com>
2018-09-25 20:52:15 +0200

    new pages + update css 25-9-2017

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,16 @@
 		return false;
 	});
 
+	$(document).on('click', '.data-list .tick-all', function() {
+		var th = $(this);
+		th.closest('.data-list').find('.tick-this').prop('checked', th.prop('checked'));
+	});
+
+	$(document).on('click', '.product-cat-dropdown .dropdown-menu a', function() {
+		var th = $(this);
+		th.closest('.product-cat-dropdown').find('button .category').text(th.text());
+	});
+
 	$('.ghost-placeholder').on('focus', function() {
 		var th = $(this);
 		th.data('placeholder', th.attr('placeholder'));
@@ -114,6 +124,7 @@
 
 	});
 
+	// Clock
 	function updateClockDisplay() {
 		var text = '',
 			time = new Date(),
@@ -136,6 +147,163 @@
 		var hours = leadingZero(Math.floor(offset / 60));
 		var minutes = leadingZero(offset % 60);
 		return sign + hours + ':' + minutes;
+	}
+
+	// Status
+	$(document).on('click', '.online-status a', function() {
+		var th = $(this),
+			current = th.closest('.online-status').find('button'),
+			backup;
+		if( current.hasClass('status-online') ) {
+			// Turn to offline mode
+		} else {
+			// Turn to online mode
+		}
+
+		// Swap the status
+		backup = current.children('span').html();
+		current.toggleClass('status-online status-offline').children('span').html(th.html());
+		th.toggleClass('status-online status-offline').html(backup);
+		return false;
+	});
+
+	// Cart on sell page
+	$(document).on('click', '.cart-root tbody .remove-product', function() {
+		var th = $(this),
+			tbody = th.closest('.cart-table').find('tbody');
+		th.closest('tr').remove();
+		if( tbody.children().length == 0 ) {
+			tbody.append('<tr><td colspan="6" class="empty"></td></tr>');
+		}
+		update_cart();
+		return false;
+	});
+
+	// Adding dummy id & price for now, should be dynamic later
+	$('.product-grid .product').each(function(n) {
+		$(this).attr('data-price', Math.floor((Math.random() * 100) + 50));
+		$(this).attr('data-code', n);
+	});
+
+	$(document).on('click', function(e) {
+		if( $(e.target).parents('.search-products').length === 0 && !$(e.target).is('.search-products') ) {
+			$('.search-results-viewer').hide();
+		}
+	});
+
+	$(document).on('focus', '.search-products', function() {
+		$('.search-results-viewer').show();
+	});
+
+	$(document).on('keyup', '.search-products input', function() {
+		var search = $(this).val().toLowerCase().trim();
+		$('.search-results-viewer').removeClass('show').children('.empty').hide();
+		$('.search-results-viewer').children(':not(.empty)').remove();
+		if( search == '' ) {
+			return;
+		}
+		var result = $('.product-grid .product').filter(function() {
+			// add additional search criteria if needed
+			var nameMatch = $(this).find('.title').text().toLowerCase().indexOf(search) > -1,
+				IDmatch = $(this).attr('data-code').toLowerCase().indexOf(search) > -1;
+
+			console.log( $(this).find('.title').text(), $(this).attr('data-code') );
+			return nameMatch || IDmatch;
+		});
+
+		if( result.length > 0 ) {
+			result.each(function() {
+				var th = $(this);
+				var html = '<a class="dropdown-item" href="#" data-code="' + th.data('code') + '">' + th.children('a').html();
+				html += '<span class="price">Price: ' + th.data('price') + ' $</span>';
+				html += '<span class="code">Code: ' + th.data('code') + '</span></a>';
+				$('.search-results-viewer').append(html.replace('"title"', '"product-title"'));
+			});
+			$('.search-results-viewer').addClass('show');
+		} else {
+			$('.search-results-viewer').addClass('show').children('.empty').show();
+		}
+	});
+
+	$(document).on('click', '.search-results-viewer a', function() {
+		var th = $(this);
+		add_product( $('.product-grid .product[data-code="' + th.data('code') + '"]') );
+		return false;
+	});
+
+	$(document).on('click', '.product a', function() {
+		var th = $(this);
+		add_product( th.parent() );
+		return false;
+	});
+
+	$(document).on('click', '.clear-cart', function() {
+		$('.cart-table tbody').html('<tr><td colspan="6" class="empty"></td></tr>');
+		update_cart();
+	});
+
+	function add_product( el ) {
+		var cart = $('.cart-table tbody'),
+			id = el.data('code'),
+			price = el.data('price'),
+			existing = cart.children('[data-id="' + id + '"]'),
+			html;
+		cart.find('.empty').closest('tr').remove();
+		if( existing.length == 0 ) {
+			html = '<tr data-id="' + id + '"><td class="remove-product"><a href="#"><i class="fa fa-times"></i></a></td>';
+			html += '<td class="item-name">' + el.find('.title').text() + '</td>';
+			html += '<td class="item-code">' + id + '</td>';
+			html += '<td class="item-qty" data-qty="1">1</td>';
+			html += '<td class="item-price">' + price + '</td>';
+			html += '<td class="item-total" data-total="' + price + '">' + price + '</td>';
+			cart.append(html);
+		} else {
+			var qty = existing.find('.item-qty').data('qty') + 1;
+			existing.find('.item-qty').data('qty', qty);
+			existing.find('.item-qty').text(qty);
+			existing.find('.item-total').data('total', price*qty);
+			existing.find('.item-total').text(price*qty);
+		}
+		update_cart();
+	}
+
+	function update_cart() {
+		var total = 0,
+			price, totalQty;
+
+		totalQty = 0;
+		$('.cart-table tbody .item-total').each(function() {
+			var th = $(this);
+			price = parseFloat( th.data('total') );
+			th.text(format_number(price) + ' $');
+			total += price;
+			price = parseFloat( th.prev().text() );
+			th.prev().text(format_number(price) + ' $');
+			totalQty += th.siblings('.item-qty').data('qty');
+		});
+		$('.cart-root .total-amount').text(format_number(total) + ' $');
+		$('.current-sale .number').text('(' + totalQty + ')');
+
+		if( total == 0 ) {
+			$('.pay-btn').prop('disabled', true);
+		} else {
+			$('.pay-btn').prop('disabled', false);
+		}
+	}
+
+	// Number formater
+	function format_number( num, digits ) {
+		var units = [' k', ' M', ' G', ' T', ' P', ' E', ' Z', ' Y'],
+		decimal;
+		digits = typeof digits == 'undefined' ? 1 : digits;
+		for( var i = units.length-1; i >= 0; i-- ) {
+			decimal = Math.pow( 1000, i+1 );
+
+			if( num <= -decimal || num >= decimal ) {
+				return +(num / decimal).toFixed(digits) + units[i];
+			}
+		}
+		return num;
 	}
 
 })(jQuery);
